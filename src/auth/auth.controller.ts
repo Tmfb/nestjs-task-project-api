3b056345da41cd1ebf83a1c0bedbd159fed4bf8a
Tmfb <of.carlos.lopez@gmail.com>
2023-01-27 12:42:59 +0100

    Added sing up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { Body } from '@nestjs/common/decorators';
+import { Post } from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  singUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.singUp(authCredentialsDto);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersRepository } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([UsersRepository, User])],
   controllers: [AuthController],
+  providers: [AuthService, UsersRepository],
 })
 export class AuthModule {}
