c6c7e837aa626ad00a650d0a623d4710b5966de3
Tmfb <of.carlos.lopez@gmail.com>
2023-01-27 13:15:40 +0100

    Added unique users and exception handling

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common/exceptions';
 import { DataSource, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -12,6 +16,16 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code == 23505) {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+      console.log(error.code);
+    }
   }
 }
