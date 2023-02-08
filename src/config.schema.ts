5c49440478022b60fd1d20fcc4d089fc8532bdcd
Tmfb <of.carlos.lopez@gmail.com>
2023-02-08 11:44:42 +0100

    Jwt secret is now provided by configModule

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -6,4 +6,5 @@ export const configValidationSchema = Joi.object({
   DB_PORT: Joi.number().default(5432).required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
 });
