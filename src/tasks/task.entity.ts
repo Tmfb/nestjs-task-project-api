0a95f8dc3246dae524698c62125e013311dd2107
Tmfb <of.carlos.lopez@gmail.com>
2023-02-23 01:05:24 +0100

    Removed task-project relationship

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -2,7 +2,6 @@ import { Exclude } from 'class-transformer';
 import { User } from '../auth/user.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
-import { Project } from '../projects/project.entity';
 
 @Entity()
 export class Task {
@@ -21,8 +20,4 @@ export class Task {
   @ManyToOne((_type) => User, (user) => user.tasks)
   @Exclude({ toPlainOnly: true })
   user: User;
-
-  @ManyToOne((_type) => Project, (project) => project.tasks)
-  @Exclude({ toPlainOnly: true })
-  project: Project;
 }
