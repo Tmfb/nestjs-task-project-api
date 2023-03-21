a0b158abee719bbc73f8aaf763133ff252dfe942
Tmfb <of.carlos.lopez@gmail.com>
2023-03-21 17:26:00 +0100

    Chore: refactor code from querybuilder to .find

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { TasksRepository } from '../tasks/tasks.repository';
 import { Project } from './project.entity';
 import { ProjectsController } from './projects.controller';
 import { ProjectsRepository } from './projects.repository';
@@ -9,6 +10,7 @@ import { ProjectsService } from './projects.service';
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProjectsRepository, Project]),
+    TypeOrmModule.forFeature([TasksRepository, Project]),
     AuthModule,
   ],
   controllers: [ProjectsController],
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,28 +6,57 @@ import { TasksRepository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { Result, ResultStates } from '../result.dto';
+import { ProjectsRepository } from '../projects/projects.repository';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(TasksRepository)
-    private taskRepository: TasksRepository,
+    private tasksRepository: TasksRepository,
+    @InjectRepository(ProjectsRepository)
+    private projectsRepository: ProjectsRepository,
   ) {}
 
   async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Result> {
-    return this.taskRepository.getTasks(filterDto, user);
+    return this.tasksRepository.getTasks(filterDto, user);
   }
 
   async getTaskById(id: string, user: User): Promise<Result> {
-    return await this.taskRepository.getTaskById(id, user);
+    let foundTask;
+    // Fetch task
+
+    try {
+      foundTask = await this.tasksRepository.find({
+        where: [
+          { id: id, admin: user },
+          { id: id, resolver: user },
+        ],
+        relations: { project: true },
+      });
+    } catch (error) {
+      return new Result(ResultStates.ERROR, {
+        message: error.message,
+        statusCode: error.statusCode,
+      });
+    }
+    // If query returns empty either auth user is not admin or task doesn't exist
+    if (!foundTask) {
+      return new Result(ResultStates.ERROR, {
+        message: `Task with id ${id} not found or you are not authorized`,
+        statusCode: HttpStatus.NOT_FOUND,
+      });
+    }
+    // Return task encapsulated in a result Object
+    return new Result(ResultStates.OK, foundTask);
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Result> {
-    return await this.taskRepository.createTask(createTaskDto, user);
+    return await this.tasksRepository.createTask(createTaskDto, user);
   }
 
   async deleteTask(id: string, user: User): Promise<Result> {
-    const removed = await this.taskRepository.delete({ id: id, admin: user });
+    const removed = await this.tasksRepository.delete({ id: id, admin: user });
 
     if (removed.affected === 0) {
       return new Result(ResultStates.ERROR, {
@@ -42,7 +71,27 @@ export class TasksService {
     status: TaskStatus,
     user: User,
   ): Promise<Result> {
-    return this.taskRepository.updateTaskStatus(id, status, user);
+    const result = await this.getTaskById(id, user);
+
+    // Check for errors retrieving the task
+    if (result.state == ResultStates.ERROR) {
+      return result;
+    }
+
+    //  Modify the encapsulated task in result.data
+    result.data.status = status;
+
+    // Update the database
+    try {
+      this.tasksRepository.save(result.data);
+    } catch (error) {
+      // Log any error and foward it to the controller encapsulated in a Result
+      return new Result(ResultStates.ERROR, {
+        message: error.message,
+        statusCode: error.statusCode,
+      });
+    }
+    return result;
   }
 
   async updateTaskResolver(
@@ -50,6 +99,79 @@ export class TasksService {
     resolver: string,
     user: User,
   ): Promise<Result> {
-    return this.taskRepository.updateTaskResolver(id, resolver, user);
+    return this.tasksRepository.updateTaskResolver(id, resolver, user);
+  }
+
+  async updateTaskProject(
+    id: string,
+    projectId: string,
+    user: User,
+  ): Promise<Result> {
+    let foundTask, foundProject;
+
+    // Fetch Task
+    try {
+      foundTask = await this.tasksRepository.find({
+        where: { id: id, admin: user },
+        relations: {
+          project: true,
+        },
+      });
+    } catch (error) {
+      return new Result(ResultStates.ERROR, {
+        message: error.message,
+        statusCode: error.statusCode,
+      });
+    }
+
+    // If query returns empty either auth user is not admin or task doesn't exist
+    if (!foundTask) {
+      return new Result(ResultStates.ERROR, {
+        message: `Task with id ${id} not found or you dont have permision to modify it's project`,
+        statusCode: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    // Fetch Project
+    try {
+      foundProject = await this.projectsRepository.find({
+        where: { id: projectId },
+        relations: { tasks: true },
+      });
+    } catch (error) {
+      return new Result(ResultStates.ERROR, {
+        message: error.message,
+        statusCode: error.statusCode,
+      });
+    }
+
+    // If query returns empty project doesn't exist
+    if (!foundProject) {
+      return new Result(ResultStates.ERROR, {
+        message: `Project with id ${projectId} not found`,
+        statusCode: HttpStatus.NOT_FOUND,
+      });
+    }
+
+    // Check if task already belongs to the project
+    if (
+      foundProject.tasks.some((task: Task) => {
+        return task.id == foundTask.id;
+      })
+    ) {
+      return new Result(ResultStates.ERROR, {
+        message: `Task with id ${id} belongs already to project ${projectId}`,
+        statusCode: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    //
+    console.log(foundTask);
+    console.log(foundProject);
+    console.log('espace');
+    foundProject.tasks.push(foundTask);
+    console.log(foundProject);
+
+    return this.updateTaskProject(id, projectId, user);
   }
 }
